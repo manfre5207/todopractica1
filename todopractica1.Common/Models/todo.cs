a190cc82f5cfdadb84ed0965080dc8ae951a6a62
LUIS <LUIS@DESKTOP-SRHR68Q>
2021-08-22 13:05:20 -0500

    Add Entity

---
+++ todopractica1.Common/Models/todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace todopractica1.Common.Models
+{
+    public class todo
+    {
+        public DateTime CreatedTime { get; set; }
+        public string TaskDescription { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ todopractica1.Common/Responses/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace todopractica1.Common.Responses
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
